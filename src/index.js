c319e3dbd8c03dadfca4379a8ed22d7543450e91
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-05-30 16:40:36 -0700

    test

--- src/index.js
+++ src/index.js
@@ -1,40 +1,47 @@
-// import searchBar from './app/src/searchBar.js'
-// import {fetchPhysics} from './src/api.js';
+// import searchBar from './src/searchBar.js'
+import {fetchPhysics} from './src/physics.js';
+import {fetchEconomics} from './src/economics.js';
+import {fetchMath} from './src/math.js';
+import {fetchComputerScience} from './src/cs.js';
+import {fetchElectricalEngineering} from './src/electricalengineering.js';
+import {fetchStatistics} from './src/statistics.js';
+import {fetchQuantBio} from './src/quantBio.js';
+import {fetchQuantFinance} from './src/quantFinance.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("its working");
 
-    document.getElementById('economicsButton').addEventListener('click', function() {
-      fetchEconomics();
-    });
+    // document.getElementById('economicsButton').addEventListener('click', function() {
+    //   fetchEconomics();
+    // });
 
-    document.getElementById('mathButton').addEventListener('click', function() {
-      fetchMath();
-    });
+    // document.getElementById('mathButton').addEventListener('click', function() {
+    //   fetchMath();
+    // });
 
-    document.getElementById('csButton').addEventListener('click', function() {
-      fetchComputerScience();
-    });
+    // document.getElementById('csButton').addEventListener('click', function() {
+    //   fetchComputerScience();
+    // });
 
-    document.getElementById('physicsButton').addEventListener('click', function() {
-      fetchPhysics();
-    });
+    // document.getElementById('physicsButton').addEventListener('click', function() {
+    //   fetchPhysics();
+    // });
 
-    document.getElementById('eeButton').addEventListener('click', function() {
-      fetchElectricalEngineering();
-    });
+    // document.getElementById('eeButton').addEventListener('click', function() {
+    //   fetchElectricalEngineering();
+    // });
 
-    document.getElementById('statsButton').addEventListener('click', function() {
-      fetchStatistics();
-    });
+    // document.getElementById('statsButton').addEventListener('click', function() {
+    //   fetchStatistics();
+    // });
 
-    document.getElementById('quantBioButton').addEventListener('click', function() {
-      fetchQuantBio();
-    });
+    // document.getElementById('quantBioButton').addEventListener('click', function() {
+    //   fetchQuantBio();
+    // });
   
-    document.getElementById('quantFinanceButton').addEventListener('click', function() {
-      fetchQuantFinance();
-    });
+    // document.getElementById('quantFinanceButton').addEventListener('click', function() {
+    //   fetchQuantFinance();
+    // });
     
     
   
